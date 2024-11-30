f28edab92c666a9ee42327d725a73b9a37c5d714
Maubek8 <mauriciokraemer@gmail.com>
2024-11-30 12:22:26 -0300

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<div class="container mt-4">
+    <footer class="text-center">
+        <p>&copy; <?php echo date('Y'); ?> NutriFit Pro. Todos os direitos reservados.</p>
+    </footer>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo ASSETS_URL; ?>js/scripts.js"></script>
+</body>
+</html>
